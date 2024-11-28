9358c6dfcb70a66b33f643617bf3e443ec9c69e8
Olena-Voitsekhivska-DH <163820044+Olena-Voitsekhivska-DH@users.noreply.github.com>
2024-11-28 12:51:07 +0200

    config

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import { AppModule } from './app/app.module';
 
 import * as Sentry from "@sentry/angular";
 
+import { environment } from './environments/environment.dev';
+
 Sentry.init({
   dsn: "https://0cf153d9e22052d2044c63a822868360@o4508375353982976.ingest.de.sentry.io/4508375358177360",
+  environment: environment.name,
   integrations: [
     Sentry.browserTracingIntegration(),
     Sentry.replayIntegration(),
@@ -13,7 +16,7 @@ Sentry.init({
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  tracePropagationTargets: ["localhost", "https://sentry-test-h22wuc6l1-olenkavoitsekhivskas-projects.vercel.app/"],
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
